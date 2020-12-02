b4ccfff3924d5732a65dd9e7e6fe80e90b8d942c
DanielRSnell <drsnell711@gmail.com>
2020-12-02 05:29:42 -0600

    first push

---
+++ gatsby-config.js
@@ -0,0 +1,60 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: `Daniel R. Snell | Product Growth Engineer`,
+    author: `Daniel Snell`,
+    description: `With 10+ years of experience in various industries, I help tech
+              startups from idea phase to high volumes. I design and build
+              scalable solutions, implement internal processes that fit your
+              organization, build teams that produce results.`,
+    siteUrl: `https://DanielSnell.Dev/`,
+    social: {
+      twitter: `SavvyHustlers`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Daniel Snell | Growth Engineer`,
+        short_name: `CV`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#38B2AC`,
+        display: `standalone`,
+        icon: 'src/assets/site-icon.png',
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    'gatsby-plugin-netlify-cms',
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        tailwind: true, // Enable tailwindcss support
+      },
+    },
+  ],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    colors: {
+      primary: colors.purple,
+      secondary: colors.gray,
+      neutral: colors.gray,
+      white: '#FFF',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
